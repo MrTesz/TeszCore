ec3c444dd9d2ffad398e9b5e255da92ac710b92b
MrTesz <mrteszoriginal@gmail.com>
2026-06-30 15:22:36 +0200

    *Docs* ~ fix base dir

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "TeszCore",
   description: "Docs",
+  base: '/TeszCore/',
   cleanUrls: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
